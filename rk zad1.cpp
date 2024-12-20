f373aae1cc2a21e537573775e9a4698dfb421ea7
KonstantinFessaliysky <kfessalijskij@gmail.com>
2024-12-20 13:17:34 +0300

    Add files via upload

---
+++ rk zad1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+    int A = 0b101101;
+    int B = 0b110011;
+    int C= A & B;
+    cout << C << endl;
+    cout << bitset<8>(A + B);
+}
